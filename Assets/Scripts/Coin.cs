acb7cbb9328b7447ee6d0707199e604543420963
Ronfreddy <eonngyiyong@gmail.com>
2023-08-21 14:14:53 +0800

    enemy attack & hitbox

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private int coinAmount = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCoinAmount(int amount)
+    {
+        coinAmount = amount;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerStat>().AddCoin(coinAmount);
+            Destroy(gameObject);
+        }
+    }
+}
