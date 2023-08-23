f8082b726f5c8349ecc4915dd0c7c625a0e83dc1
CallMeWho <lewxiuzhen@gmail.com>
2023-08-23 11:13:35 +0800

    Test Added: Angel Animation (Failed)
    -- this test is created in colosseum file
    
    -- problem: angel didn't move or do animation.
    -- reason: maybe because no joints, surface, rig hips added. need help.

--- Assets/Test/PlayerCon.cs
+++ Assets/Test/PlayerCon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement; //(xiu zhen added)
 
 public class PlayerCon : MonoBehaviour
 {
