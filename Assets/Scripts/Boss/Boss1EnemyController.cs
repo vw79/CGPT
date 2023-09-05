7fd4ff68a43a5ad4aca014148d9aa903af88c3d7
Ronfreddy <eonngyiyong@gmail.com>
2023-09-05 15:23:00 +0800

    Boss 1 Template done

---
+++ Assets/Scripts/Boss/Boss1EnemyController.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss1EnemyController : MonoBehaviour
+{
+    [Header("Animation State Names")]
+    [SerializeField] private string walkAnimation = "Boss 1 Walk";
+    [SerializeField] private string runAnimation = "Boss 1 Run";
+    [SerializeField] private string attackAnimation = "Boss 1 Attack";
+    [SerializeField] private string deadAnimation = "Boss 1 Dead";
+
+    [SerializeField] private Transform[] waypoints = new Transform[2];
+    [SerializeField] private float attackActiveTime;
+    [SerializeField] private float hitboxDuration;
+    [SerializeField] private float attackInterval;
+
+    // Enemy stats
+    private float movementSpeed;
+    private float attackPower;
+
+    private bool isDead = false;
+    private bool isAttacking = false;
+
+    // References to outer objects
+    private Transform player;
+    private Transform currentTarget;
+    private NavMeshAgent navMesh;
+    private Animator animator;
+    [SerializeField] private GameObject hitbox;
+    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] private int coinAmount = 1;
+    [SerializeField] private GameObject enemy;
+
+    private void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        currentTarget = waypoints[0];
+
+        animator = GetComponent<Animator>();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        GetStat();
+
+        switch (GetComponent<Boss1EnemyStateController>().GetEnemyState())
+        {
+            case EnemyState.Patrol:
+                Patrol();
+                break;
+            case EnemyState.Chase:
+                Chase();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Dead:
+                Dead();
+                break;
+        }
+    }
+
+    private void GetStat()
+    {
+        movementSpeed = GetComponent<CharacterStat>().GetMovementSpeed();
+        attackPower = GetComponent<CharacterStat>().GetAttackDamage();
+    }
+
+    private void Attack()
+    {
+        navMesh.speed = 0;
+        transform.LookAt(new Vector3(currentTarget.position.x, transform.position.y, currentTarget.position.z));
+        animator.Play(attackAnimation);
+        if (!isAttacking)
+        {
+            StartCoroutine(AttackActivate());
+        }
+    }
+
+    private IEnumerator AttackActivate()
+    {
+        isAttacking = true;
+        yield return new WaitForSeconds(attackActiveTime);
+        hitbox.GetComponent<IHitbox>().SetUpDamage(attackPower, hitboxDuration);
+        hitbox.SetActive(true);
+        StartCoroutine(ResetAttack());
+    }
+
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(attackInterval);
+        isAttacking = false;
+    }
+
+    private void Chase()
+    {
+        navMesh.speed = movementSpeed * 3;
+        currentTarget = player;
+        transform.LookAt(new Vector3(currentTarget.position.x, transform.position.y, currentTarget.position.z));
+        animator.Play(runAnimation);
+    }
+
+    private void Patrol()
+    {
+        navMesh.speed = movementSpeed;
+        navMesh.SetDestination(currentTarget.position);
+        animator.Play(walkAnimation);
+
+        if (currentTarget == player)
+        {
+            currentTarget = waypoints[0];
+        }
+
+        // Change waypoint if enemy has reached current waypoint
+        if (Vector3.Distance(transform.position, currentTarget.position) < 1f)
+        {
+            if (currentTarget == waypoints[0])
+            {
+                currentTarget = waypoints[1];
+            }
+            else
+            {
+                currentTarget = waypoints[0];
+            }
+        }
+    }
+
+    private void Dead()
+    {
+        isDead = true;
+        navMesh.speed = 0;
+        KillOtherEnemy();
+        BlowCoin();
+        animator.Play(deadAnimation);
+        GetComponent<Collider>().excludeLayers += LayerMask.GetMask("Player");
+
+        StartCoroutine(DestroyAfterAnimation());
+    }
+
+    private void KillOtherEnemy()
+    {
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            HealthSystem enemyHealth = enemy.GetComponent<HealthSystem>();
+            enemyHealth.TakeDamage(enemyHealth.GetHealth() + enemyHealth.GetShield());
+        }
+    }
+
+    private void BlowCoin()
+    {
+        GameObject coin = Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        coin.GetComponent<Coin>().SetCoinAmount(coinAmount);
+        coin.transform.position += Vector3.up;
+    }
+
+    private IEnumerator DestroyAfterAnimation()
+    {
+        yield return new WaitForSeconds(2.9f);
+        Destroy(enemy);
+    }
+
+    public float[] GetWaypointPosition()
+    {
+        float[] waypointPosition = new float[2];
+        waypointPosition[0] = waypoints[0].position.x;
+        waypointPosition[1] = waypoints[1].position.x;
+
+        return waypointPosition;
+    }
+}
---
+++ Assets/Scripts/Boss/Boss1EnemyStateController.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1EnemyStateController : MonoBehaviour
+{
+    public EnemyState enemyState;
+
+    private Transform player;
+
+    [SerializeField] private float seekRadius;
+    [SerializeField] private float attackRadius;
+
+    private bool IsAttacking = false;
+    private bool IsDead = false;
+
+    private float minLimitX;
+    private float maxLimitX;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        enemyState = EnemyState.Patrol;
+        float[] waypointPosition = GetComponent<Boss1EnemyController>().GetWaypointPosition();
+
+        minLimitX = Mathf.Min(waypointPosition);
+        maxLimitX = Mathf.Max(waypointPosition);
+    }
+
+    private void Update()
+    {
+        if (!IsDead)
+        {
+            switch (enemyState)
+            {
+                case EnemyState.Patrol:
+                    Patrol();
+                    break;
+                case EnemyState.Chase:
+                    Chase();
+                    break;
+                case EnemyState.Attack:
+                    Attack();
+                    break;
+                case EnemyState.Dead:
+                    Dead();
+                    break;
+            }
+        }
+    }
+
+    private void Dead()
+    {
+        IsDead = true;
+    }
+
+    public EnemyState GetEnemyState()
+    {
+        return enemyState;
+    }
+
+    private void Patrol()
+    {
+        Debug.Log("Patrol");
+        if (Vector3.Distance(transform.position, player.position) < seekRadius && player.position.x >= minLimitX && player.position.x <= maxLimitX)
+        {
+            enemyState = EnemyState.Chase;
+        }
+    }
+
+    private void Chase()
+    {
+        Debug.Log("Chase");
+        if (Vector3.Distance(transform.position, player.position) >= seekRadius || player.position.x < minLimitX || player.position.x > maxLimitX)
+        {
+            enemyState = EnemyState.Patrol;
+        }
+        else if (Vector3.Distance(transform.position, player.position) < attackRadius)
+        {
+            enemyState = EnemyState.Attack;
+        }
+    }
+
+    private void Attack()
+    {
+        if (!IsAttacking)
+        {
+            IsAttacking = true;
+
+            //Insert any attack animation
+            Debug.Log("Attack");
+
+
+            StartCoroutine(ResetAttack());
+        }
+    }
+
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(0.83f);
+        if (IsDead)
+        {
+            yield return null;
+        }
+
+        IsAttacking = false;
+
+        if (Vector3.Distance(transform.position, player.position) > attackRadius)
+        {
+            enemyState = EnemyState.Chase;
+        }
+        else if (Vector3.Distance(transform.position, player.position) > seekRadius)
+        {
+            enemyState = EnemyState.Patrol;
+        }
+    }
+
+    public void OnDeath()
+    {
+        enemyState = EnemyState.Dead;
+    }
+}
